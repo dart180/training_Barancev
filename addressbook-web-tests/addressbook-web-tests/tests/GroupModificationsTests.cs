8427fe78b741dd6ec78fdc864cc07b0d99c995ad
ahmatyanov <ahmatyanov@skbkontur.ru>
2020-02-03 00:35:22 +0500

    video 2.5, add new test no recorder

---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupModificationsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupModificationsTests : TestBase
+    {
+        [Test]
+        public void GroupModificationsTest()
+        {
+            GroupData newData = new GroupData("zzz");
+            newData.Header = "ttt";
+            newData.Footer = "qqq";
+
+            app.Groups.Modify(1, newData);
+        }
+    }
+}
