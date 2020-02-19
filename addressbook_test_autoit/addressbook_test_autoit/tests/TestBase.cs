b74230a11600164355b44f1a7db7678d3d969a55
ahmatyanov <ahmatyanov@skbkontur.ru>
2020-02-20 00:44:11 +0500

    видео 8.1

---
+++ addressbook_test_autoit/addressbook_test_autoit/tests/TestBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Policy;
+using NUnit.Framework;
+
+namespace addressbook_test_autoit
+{
+    public class TestBase
+    {
+        public ApplicationManager app;
+
+        [TestFixtureSetUp]
+        public void initApplication()
+        {
+            app = new ApplicationManager();
+        }
+
+        [TestFixtureTearDown]
+        public void stopApplication()
+        {
+            app.Stop();
+        }
+    }
+}
