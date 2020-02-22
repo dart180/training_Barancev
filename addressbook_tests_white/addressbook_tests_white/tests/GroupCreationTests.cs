825cc5c7929b64608ffc9b00970490f005c8b160
ahmatyanov <ahmatyanov@skbkontur.ru>
2020-02-22 16:26:40 +0500

    видео 8.2, white

---
+++ addressbook_tests_white/addressbook_tests_white/tests/GroupCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "white"
+            };
+
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups,newGroups);
+        }
+    }
+}
