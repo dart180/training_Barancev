06f52700df132b063bbf44f2d69dc35f8815c1f5
ahmatyanov <ahmatyanov@skbkontur.ru>
2020-02-23 11:28:00 +0500

    видео 9.1, с фтп сервером не разбирался

---
+++ mantis-tests/mantis-tests/appmanager/ApplicationManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using static System.Net.WebRequestMethods;
+
+namespace mantis_tests
+{
+    public class ApplicationManager
+    {
+        public IWebDriver driver;
+        public string baseURL;
+
+        public RegistrationHelper Registration { get; private set; }
+        public FtpHelper Ftp { get; private set; }
+
+        private static ThreadLocal<ApplicationManager> app = new ThreadLocal<ApplicationManager>();
+
+        private ApplicationManager()
+        {
+            //FirefoxOptions options = new FirefoxOptions();
+            //options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox_Esr\firefox.exe";
+            //options.UseLegacyImplementation = true;
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost:8080/";
+            Registration = new RegistrationHelper(this);
+            Ftp = new FtpHelper(this);
+        }
+
+        ~ApplicationManager()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        public static ApplicationManager GetInstance()
+        {
+            if (!app.IsValueCreated)
+            {
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.driver.Url = "http://localhost/mantisbt-1.2.17/login_page.php";
+                app.Value = newInstance;
+            }
+            return app.Value;
+        }
+
+        public IWebDriver Driver
+        {
+            get { return driver; }
+        }
+    }
+}
---
+++ mantis-tests/mantis-tests/tests/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using NUnit.Framework;
+
+namespace mantis_tests
+{
+    public class TestBase
+    {
+        public static bool PERFORM_LONG_UI_CHECKS = false;
+        protected ApplicationManager app;
+
+        [TestFixtureSetUp]
+        public void SetupApplicationManager()
+        {
+            app = ApplicationManager.GetInstance();
+        }
+
+        public static Random rnd = new Random();
+        public static string GenerateRandomString(int max)
+        {
+            int l = Convert.ToInt32(rnd.NextDouble() * max);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < l; i++)
+            {
+                builder.Append(Convert.ToChar(32 + Convert.ToInt32(rnd.NextDouble() * 65)));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
